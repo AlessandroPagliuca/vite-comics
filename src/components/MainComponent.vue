9ff223c31c8b43a363073c54a71f90e15197ebc6
AlessandroPagliuca <alessandro2ap3@gmail.com>
2023-04-13 16:37:27 +0200

    Upgraded MainComponent

---
+++ src/components/MainComponent.vue
@@ -0,0 +1,20 @@
+<template>
+    <main>
+        <div class="c92Content bg-dark text-white p-3">
+             -- Content goes here -- 
+            </div>
+
+        <!--In questo punto verrà aggiunto il componente delle icone-->
+
+    </main>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
