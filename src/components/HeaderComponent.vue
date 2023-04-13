6c6e0d5101f93632f7c87f9ebc8acff0c67b2630
AlessandroPagliuca <alessandro2ap3@gmail.com>
2023-04-13 15:29:46 +0200

    Upgraded with img, package and components app

---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,21 @@
+<template>
+    <header class="d-flex justify-content-between align-items-center">
+        <img src="../assets/images/dc-logo.png" alt="logo-dc">
+
+        <ul>
+            <li>
+                
+            </li>
+        </ul>
+    </header>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
