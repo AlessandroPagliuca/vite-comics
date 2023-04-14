55360c399cb7b641c717bd3372c2fffb1ecf526d
AlessandroPagliuca <alessandro2ap3@gmail.com>
2023-04-14 15:16:29 +0200

    Upgraded MainComponent with cardComics

--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -2,16 +2,31 @@
     <main>
         <div class="bg-dark">
             <div class="container text-white p-3">
-                -- Content goes here -- 
+                <div class="row">
+                    <div class="col-12 col-sm-6 col-md-4 col-lg-3 gap-2 d-flex justify-content-center align-items-center" v-for="(card, c) in cardComics" :key="c">
+                        <div style="width: 10rem;">
+                            <img class="card-img-top" :src="card.thumb" alt="">
+                            <div class="card-body">
+                                <p class="card-text ">{{ card.series }}</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </main>
 </template>
 
 <script>
+import {comics} from '../data/data.js'
     export default {
         name: 'MainComponent',
         components:{
+        },
+        data(){
+            return{
+                cardComics: comics,
+            }
         }
 
         
