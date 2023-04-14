f15c9679a38b0db35bd65cc42223e40feb619c72
AlessandroPagliuca <alessandro2ap3@gmail.com>
2023-04-14 18:29:32 +0200

    Upgrade HeaderLink position and style

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,13 +1,17 @@
 <template>
     <header>
-        <div class="d-flex justify-content-between align-items-center container p-3">
-            <img src="../assets/images/dc-logo.png" alt="logo-dc">
+        <div class="d-flex justify-content-between container">
+            <div class="c92HeaderImg align-self-center">
+                <img src="../assets/images/dc-logo.png" alt="logo-dc">
+            </div>
 
-            <ul class="d-flex gap-2">
+            <ul class="d-flex">
                 <!-- Andremo ad inserire il => v-for=" (link, index ) in links" :key="index" <= 
                 all'interno del tag li per generare la lista completa dei link  -->
                 <li v-for="link in linksNav">
-                    <a class="text-dark text-uppercase fw-semibold">{{ link.navLink }}</a>
+                    <div class="c92LinkBox p-2 d-flex align-items-center">
+                        <a class="text-dark text-uppercase fw-semibold ">{{ link.navLink }}</a>
+                    </div>
                 </li>
             </ul>
         </div> 
@@ -29,5 +33,37 @@
 </script>
 
 <style lang="scss" scoped>
+    header{
+        width: 100%;
+        height: 100px;
+        background-color: white;
+        position: fixed;
+        display: flex;
+        z-index: 1000;
+
+        .c92HeaderImg{
+            width: 80px;
+            height: 80px;
+
+            img{
+                width: 100%;
+                height: 100%;
+            }
+        };
+        .c92LinkBox{
+            height: 100px;
+            cursor: pointer;
+
+            a{
+                text-decoration: none;
+            };
+
+            &:hover{
+                border-bottom: 5px solid blue;
+
+            }
+        }
+
+    }
 
 </style>
