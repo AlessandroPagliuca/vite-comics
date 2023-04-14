d5e402d13e6955396867a96ef6ef76c4d1bfe00c
AlessandroPagliuca <alessandro2ap3@gmail.com>
2023-04-14 17:41:41 +0200

    Upgraded navLink

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -3,11 +3,11 @@
         <div class="d-flex justify-content-between align-items-center container p-3">
             <img src="../assets/images/dc-logo.png" alt="logo-dc">
 
-            <ul>
+            <ul class="d-flex gap-2">
                 <!-- Andremo ad inserire il => v-for=" (link, index ) in links" :key="index" <= 
                 all'interno del tag li per generare la lista completa dei link  -->
-                <li>
-                    link
+                <li v-for="link in linksNav">
+                    <a class="text-dark text-uppercase fw-semibold">{{ link.navLink }}</a>
                 </li>
             </ul>
         </div> 
@@ -15,9 +15,15 @@
 </template>
 
 <script>
+
+    import {navLinks} from '../data/data.js';
     export default {
         name: 'HeaderComponent',
-
+        data(){
+            return{
+                linksNav: navLinks,
+            }
+        }
         
     }
 </script>
